623e53c3cd09326ce728d7424a92b78f601a4dae
labco10 <labco10@naver.com>
2023-06-04 14:24:52 +0900

    상수 추가

--- lib/variable/variable.dart
+++ lib/variable/variable.dart
@@ -1,13 +1,32 @@
-void main() {
+class Variable {
 
-  print("테스트");
+  Variable(){
+    print("테스트");
 
-  //타입,변수
-  //dart에서는 4가지 타입이 존재 
-  //1.String 2.int 3.double 4.bool 
-  //문자열은 ""이 붙어야한다.
+    //타입,변수
+    //dart에서는 4가지 타입이 존재
+    //1.String 2.int 3.double 4.bool
+    //문자열은 ""이 붙어야한다.
 
 
-  String name = "류상훈";
+    String name = "류상훈";
+
+    //var 타입은 최초 입력한 타입을 인식
+    var b = 10;
+
+    //dynamic = 모든 변수 타입이 입력가능
+    //실제 개발시에 무슨값인지 알기 어려움
+    dynamic d = "류상훈2";
+    d = 12;
+    d = 22.3;
+    d = false;
+
+    final String address = "seoul";
+    const q = "ff";
+    // address = "busan";
+
+  }
+
+
 
 }
